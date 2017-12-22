2a8526851c4db9c2d1e409bf3967f77642c993c5
Cihan <adrian.towery@gmail.com>
2017-12-22 00:13:59 +0000

    added to github for your viewing pleasure

---
+++ binpostscript.js
@@ -0,0 +1,39 @@
+//Cihan Adrian Towery
+//CS290
+//AJAX
+//03 Nov 2017
+
+//alert("test")
+
+document.addEventListener("DOMContentLoaded", bindButtons);
+
+function bindButtons()
+{
+    document.getElementById("strSubmit").addEventListener("click", function(event)
+    {
+        var req = new XMLHttpRequest();
+        var pasteSite = "http://httpbin.org/post";
+        var payload =
+        {
+            "str": null
+        };
+        payload.str = document.getElementById("str").value; 
+        req.open("POST", pasteSite, true);                      
+        req.setRequestHeader("Content-Type", "application/json");
+        req.addEventListener("load",function(){
+            if(req.status >= 200 && req.status < 400)   
+            {
+                var response = JSON.parse(JSON.parse(req.responseText).data);
+                displayResult(response);
+            }
+
+        });
+        req.send(JSON.stringify(payload));
+        event.preventDefault();
+    });
+}
+
+function displayResult(response)                                     
+{
+    document.getElementById("streturn").textContent = response.str;
+}
---
+++ weather.js
@@ -0,0 +1,45 @@
+//Cihan Adrian Towery
+//CS290
+//AJAX
+//03 Nov 2017
+
+//alert("loaded");
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+
+//was unable to get proper connection. I went to the openweather site, signed up got a key, but simply cant get it to connect
+function bindButtons() {
+	document.getElementById('getWeather').addEventListener('click', function(event){
+		var req = new XMLHttpRequest();
+		var appID = "&appid=c67ba197ba5f6dec494044183d445d90"; 
+		var zip = document.getElementById("zip").value;
+        var city = document.getElementById("city").value;
+        var payload;
+        //got all this info from ipenweather site
+        //i couldnt get zip to work for a while, and i think i overloaded my appid by testing..
+        if(zip)
+		{
+            payload = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + appID;
+		    
+		}
+        else
+        {
+            payload = "http://api.openweathermap.org/data/2.5/weather?q=" + city + appID;
+        }
+		req.open("GET", payload, true);
+        
+        //stack exchange helped here
+		req.addEventListener('load', function() {
+			if (req.status >= 200 && req.status < 400) {
+				var response = JSON.parse(req.responseText);
+				displayWeather(response);
+			}
+    });
+		req.send();
+		event.preventDefault();
+});
+}
+function displayWeather(response)
+{
+    document.getElementById("temperature").textContent = response.main.temp; 
+}
